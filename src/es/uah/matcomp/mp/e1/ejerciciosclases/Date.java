48ae8721cc46e4fc31e4288400e067851a4b24dc
rober <robertrafael.rrsm@gmail.com>
2025-02-12 19:04:37 +0100

    Date definido

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Date.java
@@ -0,0 +1,43 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Date {
+    private int day; // Me falta establecer el rango de cada una
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return day + "/" + month + "/" + year;
+    }
+}
