d1a68f1dc41cb87daa8feb4adfc30be1f72f4a49
rober <robertrafael.rrsm@gmail.com>
2025-02-19 14:40:32 +0100

    Point2D&3D

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/apartadoC/Point3D.java
@@ -0,0 +1,36 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.apartadoC;
+
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+    public Point3D(){}
+
+    public float getZ() {
+        return z;
+    }
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float[] getXYZ(){
+        float[] xyz = new float[3];
+        xyz[0] = super.getX();
+        xyz[1] = super.getY();
+        xyz[2] = z;
+        return xyz;
+    }
+    public void setXYZ(float x, float y, float z){
+        super.setX(x);
+        super.setY(y);
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + super.getX() + ", " + super.getY() + ", " + z + ")";
+    }
+}
